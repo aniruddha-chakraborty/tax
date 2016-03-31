ac8ccbb776f66dd72063788151936bdad90aa8ec
Aniruddha Chakraborty <app.anik@gmail.com>
2016-03-31 20:19:45 +0600

    added seeder class

---
+++ database/seeds/BussnessAdminSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BussnessAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ database/seeds/ShopKeeperSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ShopKeeperSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
