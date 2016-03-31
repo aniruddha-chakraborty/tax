377c0e76e9b717223b11ca0b1c7b6aec35fe6eee
Aniruddha Chakraborty <app.anik@gmail.com>
2016-03-31 20:15:59 +0600

    added controller , model added

---
+++ app/Models/loginQuery.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+
+
+class loginQuery extends Model
+{
+
+
+	public function shopAdminLogin($username,$password) {
+
+		
+
+	}
+
+	public function shopKeeperLogin($username,$password){
+
+
+	}
+
+	public function rajukAdminLogin($username,$password) {
+
+
+	}
+
+	public function carAdminLogin($username,$password) {
+
+
+	}
+
+	public function policeAdminLogin($username,$password) {
+
+
+	}
+
+	public function visaAdminLogin($username,$password) {
+
+
+
+	}
+
+}
